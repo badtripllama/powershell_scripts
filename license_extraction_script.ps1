d87b91eb6d0e97a16cb2fbcb6938c5e00b25439f
badtripllama <72275683+badtripllama@users.noreply.github.com>
2023-07-09 23:33:07 +0400

    amended variables to lowercase

--- license_extraction_script.ps1
+++ license_extraction_script.ps1
@@ -9,14 +9,12 @@ $usersfromfile = Get-Content "C:\Users\jramphul\Desktop\test1.csv"
 #initialise result set
 $output = @()
 
- 
 #for each line in csv
 foreach ($u in $usersfromfile)
 {
     Write-Host "Processing $u..."
     $licenses = Get-MsolUser -UserPrincipalName $u | select Licenses -ExpandProperty Licenses
 
- 
 #for each license type in license property
     foreach ($license in $licenses)
     {
@@ -36,6 +34,5 @@ foreach ($u in $usersfromfile)
         }
     }
 }
-
  
 $output | Export-Csv -Path "C:\Users\jramphul\Desktop\exp1.csv" -NoTypeInformation
