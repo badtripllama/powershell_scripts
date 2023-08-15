cd084143552a00c5b8edc32cdd6a075e600c321d
badtripllama <72275683+badtripllama@users.noreply.github.com>
2023-08-15 11:45:23 +0400

    required class info

--- signins.ps1
+++ signins.ps1
@@ -10,7 +10,7 @@ Connect-AzureAD
 $SetDate = (Get-Date).AddDays(-7);
 $SetDate = Get-Date($SetDate) -format yyyy-MM-dd
 
-$SetSigninLogs = Get-AzureADAuditSignInLogs -Filter "createdDateTime gt $SetDate" -All $true | select userDisplayName, userPrincipalName, appDisplayName, ipAddress, clientAppUsed, @{Name = 'DeviceOS'; Expression = {$_.DeviceDetail.OperatingSystem}},@{Name = 'Location'; Expression = {$_.Location.City}}
+$SetSigninLogs = Get-AzureADAuditSignInLogs -Filter "createdDateTime gt $SetDate" -All $true | select userDisplayName, userPrincipalName, appDisplayName, IpAddress, clientAppUsed, @{Name = 'DeviceOS'; Expression = {$_.DeviceDetail.OperatingSystem}},@{Name = 'Location'; Expression = {$_.Location.City}}
 
 $SetSigninLogs | Export-Csv -Path "C:\Users\jramphul\Desktop\signins.csv" -notype
 
@@ -22,3 +22,15 @@ $SetSigninLogs | Export-Csv -Path "C:\Users\jramphul\Desktop\signins.csv" -notyp
 
 $founduserstats = Get-AzureADAuditSignInLogs -Filter "status/errorCode ne 0" -All $true | `
 Select CreatedDateTime, UserPrincipalName, AppDisplayName, IpAddress
+
+
+
+
+@{Name = 'ErrorCode'; Expression = {$_.Status.ErrorCode}},
+@{Name = 'FailureReason'; Expression = {$_.Status.FailureReason}},
+@{Name = 'DeviceOS'; Expression = {$_.DeviceDetail.OperatingSystem}},
+@{Name = 'Browser'; Expression = {$_.DeviceDetail.Browser}},
+@{Name = 'Location'; Expression = {$_.Location.CountryOrRegion}}
+
+
+
