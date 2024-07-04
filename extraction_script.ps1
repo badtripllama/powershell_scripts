32f909970d6b46aca8ef1fbbad285cd63f1ef4b3
badtripllama <72275683+badtripllama@users.noreply.github.com>
2024-07-04 16:58:58 +0400

    path corrections

--- extraction_script.ps1
+++ extraction_script.ps1
@@ -7,7 +7,7 @@ Add-PSSnapin Microsoft.Exchange.Management.Powershell.Support
 #   %windir%\System32\WindowsPowerShell\v1.0\Modules
 ImportSystemModules
 
-$usersfromfile = Get-Content "C:\Users\badtripllama\Desktop\test.csv"
+$usersfromfile = Get-Content "C:\Users\jramphul\Desktop\test.csv"
 
 $userstats = @()
 ForEach ($u in $usersfromfile)
@@ -21,9 +21,11 @@ ForEach ($u in $usersfromfile)
     $userstats = $userstats + $founduserstats
 }
 
-$userstats | Export-Csv -Path "C:\Users\badtripllama\Desktop\userstats.csv" -notype
+$userstats | Export-Csv -Path "C:\Users\jramphul\Desktop\userstats.csv" -notype
+_________________________________________________________________________________________________________________________
 
-<#
+#MX Extraction
+
 $usersfromfile = Get-Content "C:\Users\badtripllama\Desktop\test.csv"
 
 $userstats = @()
@@ -38,7 +40,24 @@ ForEach ($u in $usersfromfile)
     $userstats = $userstats + $founduserstats
 }
 
-$userstats | Export-Csv -Path "C:\Users\badtripllama\Desktop\userstats.csv" -notype
+$userstats | Export-Csv -Path "C:\Users\jramphul\Desktop\userstats.csv" -notype
 
+_________________________________________________________________________________________________________________________
 
-#>
+#TXT Extraction
+$usersfromfile = Get-Content "C:\Users\jramphul\Desktop\test.csv"
+
+$userstats = @()
+ForEach ($u in $usersfromfile)
+{
+    Write-Host "Processing $u..."
+    
+    #get cmdlet can be changed depending on needs.
+    $founduserstats = resolve-dnsname -name $u -Type txt | `
+       Select Name, @{Name='Strings';Expression={[string]::join(“;”, ($_.strings))}}
+
+    $userstats = $userstats + $founduserstats
+}
+
+$userstats | Export-Csv -Path "C:\Users\jramphul\Desktop\userstats.csv" -notype
+_________________________________________________________________________________________________________________________
