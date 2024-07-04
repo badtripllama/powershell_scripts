1ee5f4672f4a5a9a2f01fe2eeb29039ae5424c00
badtripllama <72275683+badtripllama@users.noreply.github.com>
2024-07-04 17:00:50 +0400

    delete

--- domain_extr.ps1
+++
@@ -1,5 +0,0 @@
-#If you are running this on PowerShell 7+/Core, you need to import the module in compatibility mode:
-
-Import-Module AzureAD -UseWindowsPowerShell
-
-Get-MsolDomain | Export-CSV o365domains.csv
