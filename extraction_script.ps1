abfce7c0e024393d2ddbb34c6ee19a6e5b2279d9
badtripllama <72275683+badtripllama@users.noreply.github.com>
2024-07-04 17:00:10 +0400

    saves

--- extraction_script.ps1
+++ extraction_script.ps1
@@ -23,7 +23,15 @@ ForEach ($u in $usersfromfile)
 
 $userstats | Export-Csv -Path "C:\Users\jramphul\Desktop\userstats.csv" -notype
 _________________________________________________________________________________________________________________________
+#tenant managed domains extraction
 
+#If you are running this on PowerShell 7+/Core, you need to import the module in compatibility mode:
+
+Import-Module AzureAD -UseWindowsPowerShell
+
+Get-MsolDomain | Export-CSV o365domains.csv
+
+_________________________________________________________________________________________________________________________
 #MX Extraction
 
 $usersfromfile = Get-Content "C:\Users\badtripllama\Desktop\test.csv"
